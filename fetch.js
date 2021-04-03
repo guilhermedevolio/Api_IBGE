d4cda0455c664e78895c8452e05e070982fb9bd9
Guilherme  Devólio <61203395+Guilherme5889@users.noreply.github.com>
2021-04-03 16:18:21 -0300

    Format Html and JS

--- fetch.js
+++ fetch.js
@@ -1,53 +1,58 @@
 $.ajax({
     type: 'GET',
-    url : 'https://servicodados.ibge.gov.br/api/v1/localidades/estados/',
-    success: function(callback){
+    url: 'https://servicodados.ibge.gov.br/api/v1/localidades/estados/',
+    success: function(callback) {
         showEstados(callback);
     }
 })
 
 
-function showEstados(estados){
+function showEstados(estados) {
     estados.forEach(element => {
         $('#selectEstados').append('<option selected>' + element.sigla + '</option>');
-    }); 
+    });
 }
 
-$('#selectEstados').on('change', function(){
+$('#selectEstados').on('change', function() {
     var estado = $('#selectEstados option:selected').val();
     var table = $('#datatable').DataTable();
     table.destroy();
     $('#datatable').DataTable({
         "ajax": {
-            "url": "https://servicodados.ibge.gov.br/api/v1/localidades/estados/"+estado+'/municipios',
+            "url": "https://servicodados.ibge.gov.br/api/v1/localidades/estados/" + estado + '/municipios',
             "dataSrc": ""
         },
-        "columns": [
-            {"data": "id"},
-            {"data": "nome"},
-            {"data": "microrregiao.nome"},
+        "columns": [{
+                "data": "id"
+            },
+            {
+                "data": "nome"
+            },
+            {
+                "data": "microrregiao.nome"
+            },
             {
                 "mRender": function(data, type, full) {
                     return '<p class="btn btn-success btn-sm" id="btn-view" val=' + full['id'] + '>' + 'Ver Detalhes' + '</p>';
                 }
-            }  
+            }
         ]
     })
 
     $('.tabela').show();
     $('.alert').hide();
-   
+
 })
 
 
 
-$(document).on('click', '#btn-view', function(){
+$(document).on('click', '#btn-view', function() {
     var city_id = $(this).attr('val');
     $.ajax({
-        url: 'https://servicodados.ibge.gov.br/api/v1/localidades/municipios/'+city_id,
+        url: 'https://servicodados.ibge.gov.br/api/v1/localidades/municipios/' + city_id,
         type: 'GET',
-        success: function(callback){
-         
+        success: function(callback) {
+
             $('#cityName').html(callback.nome);
             $('#cityMicro').html(callback.microrregiao.mesorregiao.nome);
             $('#cityUF').html(callback.microrregiao.mesorregiao.UF.nome);
@@ -55,5 +60,5 @@ $(document).on('click', '#btn-view', function(){
             $('#city_modal').modal('show');
         }
     })
-    
+
 });
